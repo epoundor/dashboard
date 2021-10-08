7f7cba19e4d8a2dd676c82d6d366ffbf0ea5ce6f
epoundor <91498943+epoundor@users.noreply.github.com>
2021-10-08 23:55:25 +0100

    Add files via upload

---
+++ Projet/js/main.js
@@ -0,0 +1,49 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+$(document).ready(function () {
+/*===== MENU SHOW =====*/
+        $('.nav__toggle').on("click",function(){
+            $('.nav__menu').addClass('show-menu');
+        })
+/*===== MENU HIDDEN =====*/
+        $('.nav__close').on("click",function(){
+            $('.nav__menu').removeClass('show-menu');
+        })
+/*==================== REMOVE MENU MOBILE ====================*/
+        $('.nav__link').on("click",function(){
+            $('.nav__menu').removeClass('show-menu');
+        })
+    }
+);
+/* Validate if constant exists */
+
+
+/* Validate if constant exists */
+
+
+
+
+/*==================== ACCORDION SKILLS ====================*/
+
+
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
