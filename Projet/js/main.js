bc42608e7cb008689406d1cc734b7577ff1d5a1f
epoundor <91498943+epoundor@users.noreply.github.com>
2021-10-09 04:19:49 +0100

    Add files via upload

--- Projet/js/main.js
+++ Projet/js/main.js
@@ -12,38 +12,44 @@ $(document).ready(function () {
         $('.nav__link').on("click",function(){
             $('.nav__menu').removeClass('show-menu');
         })
-    }
-);
-/* Validate if constant exists */
-
-
-/* Validate if constant exists */
-
-
-
-
+        
 /*==================== ACCORDION SKILLS ====================*/
-
-
-/*==================== QUALIFICATION TABS ====================*/
-
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+        let  Scontent=$('.skill__content');
+        let SHeader=document.querySelectorAll('.skill__header');
+        function toogleSkills(itemName) {
+            let itemNameClass=itemName.parentNode.className;
+            for(i=0; i<Scontent.length; i++){
+                Scontent[i].className=('skill__content skill__close');
+            }
+            if(itemNameClass=== 'skill__content skill__close'){
+                   itemName.parentNode.className=('skill__content skill__open');
+            }
+        }
+       SHeader.forEach(element => {
+           element.addEventListener('click', ()=> toogleSkills(element));
+        });
+        console.log(SHeader[0].parentNode.className)
+        /*==================== QUALIFICATION TABS ====================*/
+    
+        
+        /*==================== SERVICES MODAL ====================*/
+        
+        
+        /*==================== PORTFOLIO SWIPER  ====================*/
+        
+        
+        /*==================== TESTIMONIAL ====================*/
+        
+        
+        /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+        
+        
+        /*==================== CHANGE BACKGROUND HEADER ====================*/ 
+        
+        
+        /*==================== SHOW SCROLL UP ====================*/ 
+        
+        
+        /*==================== DARK LIGHT THEME ====================*/ 
+    }
+        );
